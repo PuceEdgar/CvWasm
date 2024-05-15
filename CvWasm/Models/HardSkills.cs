91910c30f6a1761c559ff97161df63d7c5a54788
puce.edgar <puce.edgar@gmail.com>
2024-05-15 14:34:21 +0900

    added skills data

--- CvWasm/Models/HardSkills.cs
+++ CvWasm/Models/HardSkills.cs
@@ -3,8 +3,8 @@
 public class HardSkills
 {
     public string[]? Programming { get; set; }
-    public string[]? Tools { get; set; }
+    public string? Tools { get; set; }
     public string[]? Other { get; set; }
-    public string[]? WayOfWorking { get; set; }
+    public string? WayOfWorking { get; set; }
     public string[]? Languages { get; set; }
 }
