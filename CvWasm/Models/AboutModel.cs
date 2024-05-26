21ec2af31f29308a2f59cb7817d5ca4f3accf9c2
puce.edgar <puce.edgar@gmail.com>
2024-05-26 14:45:22 +0900

    added json property names to aboutmodel

--- CvWasm/Models/AboutModel.cs
+++ CvWasm/Models/AboutModel.cs
@@ -1,11 +1,13 @@
-﻿namespace CvWasm.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace CvWasm.Models;
 
 public record AboutModel(
-    string? FullName, 
-    string? DateOfBirth, 
-    string? Nationality, 
-    string? Email, 
-    string? GitHubLink, 
-    string? LinkedInLink, 
-    string? PersonalStatement
+   [property: JsonPropertyName("fullName")] string? FullName,
+   [property: JsonPropertyName("dateOfBirth")] string? DateOfBirth,
+   [property: JsonPropertyName("nationality")] string? Nationality,
+   [property: JsonPropertyName("email")] string? Email,
+   [property: JsonPropertyName("gitHubLink")] string? GitHubLink,
+   [property: JsonPropertyName("linkedInLink")] string? LinkedInLink,
+   [property: JsonPropertyName("personalStatement")] string? PersonalStatement
     );
