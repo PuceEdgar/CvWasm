1b472679d955ce4fb011d09a762d45eec3b3b5d0
puce.edgar <puce.edgar@gmail.com>
2024-05-28 18:49:37 +0900

    added tests for table header and data row component

--- CvWasm.Tests/ComponentTests.cs
+++ CvWasm.Tests/ComponentTests.cs
@@ -1,5 +1,6 @@
 using Bunit;
 using CvWasm.Pages;
+using CvWasm.SharedComponents;
 
 namespace CvWasm.Tests;
 
@@ -27,5 +28,26 @@ public class ComponentTests : TestContext
             );
 
         cut.Find("p").MarkupMatches($"<p>{message}</p>");
+    }
+
+    [Fact]
+    public void TableHeaderAndDataRowComponentDisplaysHeaderAndData()
+    {
+        string header = "header";
+        string data = "data";
+        var cut = RenderComponent<TableHeaderAndDataRow>(
+            parameters => parameters.Add(p => p.TableHeader, header).Add(p => p.TableData, data)
+            );
+
+        var tr = cut.Find("tr");
+        var th = cut.Find("th");
+        var td = cut.Find("td");
+
+        Assert.Equal(2, tr.Children.Length);
+        Assert.Equal(th, tr.Children[0]);
+        Assert.Equal(td, tr.Children[1]);
+
+        th.MarkupMatches($"<th>{header}</th>");
+        td.MarkupMatches($"<td>{data}</td>");
     }
 }
