44307ac5af763f172a9e43894c02418725d8c6c3
puce.edgar <puce.edgar@gmail.com>
2024-06-05 17:00:59 +0900

    refactoring

--- CvWasm/Managers/CommandService.cs
+++ CvWasm/Managers/CommandService.cs
@@ -1,10 +1,13 @@
-﻿namespace CvWasm.Managers;
+﻿using CvWasm.Factory;
+
+namespace CvWasm.Managers;
 
 public class CommandService : ICommandService
 {
     private readonly IComponentRepository _componentRepository;
     private readonly IJsService _jsService;
     private readonly IFileService _fileManager;
+    private readonly string[] _componentCommands = [AboutCommand, EducationCommand, HardSkillsCommand, SoftSkillsCommand, ExperienceCommand, HelpCommand];
 
     public CommandService(IComponentRepository componentManager, IJsService jsService, IFileService fileManager)
     {
@@ -15,56 +18,34 @@ public class CommandService : ICommandService
 
     public async Task ExecuteCommand(string command)
     {
-        switch (command)
+        if (command.Equals(ClearCommand))
         {
-            case ClearCommand:
-                _componentRepository.ClearWindow();
-                break;
-            case AboutCommand:
-            case EducationCommand:
-            case HardSkillsCommand:
-            case SoftSkillsCommand:
-            case ExperienceCommand:
-            case HelpCommand:
-                AddNewComponent(command);
-                break;
-            case OpenGitHubCommand:
-                await OpenLinkInNewTab(GithubLink, command);
-                break;
-            case OpenLinkedInCommand:
-                await OpenLinkInNewTab(LinkedInLink, command);
-                break;
-            case DownloadEngCvCommand:
-                await DownloadCv(Languages.eng, command);
-                break;
-            case DownloadKorCvCommand:
-                await DownloadCv(Languages.kor, command);
-                break;
-            case ChangeLanguageToEnglishCommand:
-                SetLanguageTo(Languages.eng, command);
-                break;
-            case ChangeLanguageToKoreanCommand:
-                SetLanguageTo(Languages.kor, command);
-                break;
-            default:
-                AddNewComponent(command, true);
-                break;
+            _componentRepository.ClearWindow();
+            return;
         }
+
+        var component = command switch
+        {
+            var c when _componentCommands.Contains(c) => AddNewComponent(command),
+            OpenGitHubCommand => await OpenLinkInNewTab(GithubLink, command),
+            OpenLinkedInCommand => await OpenLinkInNewTab(LinkedInLink, command),
+            DownloadEngCvCommand => await DownloadCv(Languages.eng, command),
+            DownloadKorCvCommand => await DownloadCv(Languages.kor, command),
+            ChangeLanguageToEnglishCommand => SetLanguageTo(Languages.eng, command),
+            ChangeLanguageToKoreanCommand => SetLanguageTo(Languages.kor, command),
+            _ => AddNewComponent(command, true),
+        };
+
+        _componentRepository.AddComponentToList(component);
     }
 
-    private void AddNewComponent(string command, bool isError = false)
+    private BaseComponent AddNewComponent(string command, bool isError = false)
     {
-        if (isError)
-        {
-            _componentRepository.CreateNewComponentAndAddToList(command, ErrorService.GenerateBadCommandErrorMessage(command, StateContainer.CurrentSelectedLanguage));
-        }
-        else
-        {
-            _componentRepository.CreateNewComponentAndAddToList(command);
-        }
+        return isError ? _componentRepository.CreateNewComponent(command, ErrorService.GenerateBadCommandErrorMessage(command, StateContainer.CurrentSelectedLanguage))
+            : _componentRepository.CreateNewComponent(command);
     }
 
-    private async Task OpenLinkInNewTab(string url, string command)
+    private async Task<BaseComponent> OpenLinkInNewTab(string url, string command)
     {
         bool isSuccess = true;
         try
@@ -76,37 +57,35 @@ public class CommandService : ICommandService
             isSuccess = false;
         }
 
-        _componentRepository.CreateNewComponentAndAddToList(command, ErrorService.GetCommandResultMessage(isSuccess));
+        return _componentRepository.CreateNewComponent(command, ErrorService.GetCommandResultMessage(isSuccess));
     }
 
-    private async Task DownloadCv(Languages language, string command)
+    private async Task<BaseComponent> DownloadCv(Languages language, string command)
     {
         bool isSuccess = true;
         try
         {
             var base64 = await _fileManager.GetBase64FromPdfCv(language.ToString());
-            if (string.IsNullOrWhiteSpace(base64)) 
+            if (string.IsNullOrWhiteSpace(base64))
             {
                 isSuccess = false;
-                
-            } 
+            }
             else
             {
                 await _jsService.CallJsFunctionToDownloadCv(language, base64);
             }
-            
         }
         catch (Exception)
         {
             isSuccess = false;
         }
 
-        _componentRepository.CreateNewComponentAndAddToList(command, ErrorService.GetCommandResultMessage(isSuccess));
+        return _componentRepository.CreateNewComponent(command, ErrorService.GetCommandResultMessage(isSuccess));
     }
 
-    private void SetLanguageTo(Languages language, string command)
+    private BaseComponent SetLanguageTo(Languages language, string command)
     {
         StateContainer.CurrentSelectedLanguage = language;
-        _componentRepository.CreateNewComponentAndAddToList(command, ErrorService.GetCommandResultMessage(true));
+        return _componentRepository.CreateNewComponent(command, ErrorService.GetCommandResultMessage(true));
     }
 }
